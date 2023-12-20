61468e7b753dc12da5f6e00bc67affd17c955414
NiRoU <nicolas.nacho93@gmail.com>
2023-12-20 01:55:13 -0500

    rutas basicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,11 +22,9 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return ListView.builder(
       itemCount: appMenuItems.length,
-      itemBuilder: (context, index){
+      itemBuilder: (context, index) {
         final menuItem = appMenuItems[index];
         return _CustomListTile(menuItem: menuItem);
       },
@@ -42,16 +41,21 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading: Icon(menuItem.icon,color: colors.primary,),
-      trailing: Icon(Icons.arrow_forward_ios_rounded,color: colors.primary),
+      leading: Icon(
+        menuItem.icon,
+        color: colors.primary,
+      ),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){
-        //TODO navegar a otras pantallas
+      onTap: () {
+        // Navigator.of(context).push(
+        //     MaterialPageRoute(builder: (context) => const ButtonsScreen()));
+
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
